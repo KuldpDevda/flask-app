e76ac7d3d2c7c2d2b075e0a1645082ef2a220211
Kuldeep <kuldeepdevda319@gmail.com>
2024-05-24 12:51:24 +0530

    base file allowed host change

--- projectsync/settings/base.py
+++ projectsync/settings/base.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-*4=y0wq8(0i!l$)ww1#b)-!dtt^)&(4owq6@5-xcd%d%*j2ic2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 INSTALLED_APPS = [
